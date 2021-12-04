f85d036df9076605d079d681afada34c35681658
legitbeep <glorytoharsh@gmail.com>
2021-12-04 19:18:10 +0530

    init commit

---
+++ src/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { usename, user, username } from "./auth";
+
+  const signout = () => {
+    user.leave(); // gun js stuff
+    username.set(""); // remove username from store
+  };
+</script>
+
+<header>
+  <h1>Gun-Chat</h1>
+  <!--"$" is used to subscribe to the store and react to updates -->
+  {#if $username}
+    <nav class="username">
+      <h1>{$username}</h1>
+      <img
+        src={`https://avatars.dicebear.com/api/avataaars/${$username}.svg`}
+        alt={$username}
+      />
+    </nav>
+    <button class="logout-btn" on:click={signout}>Logout</button>
+  {/if}
+</header>
---
+++ src/auth.js
@@ -0,0 +1,25 @@
+import GUN from "gun";
+import "gun/sea";
+import "gun/axe";
+import { writable } from "svelte/store"; // reactive store
+
+export const db = GUN();
+
+// current user
+export const curUser = db.user().recall({ sessionStorage: true });
+
+// get user name
+// user.get("alias");
+
+// set up username in store
+export const username = writable("");
+
+// update username throughout the store
+user.get("alias").on((newName) => username.set(newName));
+
+// handle auth state
+db.on("auth", async (e) => {
+  const newName = await user.get("alias");
+  // update user
+  username.set(newName);
+});
